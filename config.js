d21712311e018d0c8dc8d266ae9497a128d668c0
Henrique <henrique@sidekick-coder.com>
2025-02-15 17:49:15 -0300

    feat: restructure ESLint configuration and update package names

--- config.js
+++ config.js
@@ -1,64 +1,96 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
-import tsEslint from 'typescript-eslint'
-import pluginVue from 'eslint-plugin-vue'
+import typescriptEslint from 'typescript-eslint'
+import eslintPluginVue from 'eslint-plugin-vue'
 import vueParser from 'vue-eslint-parser'
 import tsParser from '@typescript-eslint/parser'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
-import tailwind from 'eslint-plugin-tailwindcss'
+import eslintTailwind from 'eslint-plugin-tailwindcss'
 
-export const config = tsEslint.config(
-    { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
-    pluginJs.configs.recommended,
-    eslintPluginPrettierRecommended,
-    ...tailwind.configs['flat/recommended'],
-    ...tsEslint.configs.recommended,
-    ...pluginVue.configs['flat/recommended'],
-    {
-        languageOptions: {
-            parser: vueParser,
-            parserOptions: {
-                parser: tsParser,
-            },
-            globals: { ...globals.browser, ...globals.node },
+export const js = {
+    files: ['**/*.{js,mjs,cjs}'],
+    extends: [
+        pluginJs.configs.recommended,
+    ],
+}
+
+export const ts = {
+    files: ['**/*.{ts,mts,cts}'],
+    extends: [
+        typescriptEslint.configs.recommended,
+    ],
+}
+
+export const vue = {
+    files: ['**/*.vue'],
+    extends: [
+        pluginJs.configs.recommended,
+        ...typescriptEslint.configs.recommended,
+        ...eslintPluginVue.configs['flat/recommended'],
+    ],
+    languageOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        globals: globals.browser,
+        parserOptions: {
+            parser: typescriptEslint.parser,
         },
     },
-    {
-        rules: {
-            'no-undef': 'off',
-            '@typescript-eslint/no-var-requires': 'off',
-            '@typescript-eslint/no-explicit-any': 'off',
-            '@typescript-eslint/no-unsafe-function-type': 'off',
-            '@typescript-eslint/no-empty-object-type': 'off',
-            '@typescript-eslint/no-require-imports': 'off',
-            'vue/multi-word-component-names': 'off',
-            'vue/no-setup-props-destructure': 'off',
-            'vue/one-component-per-file': 'off',
-            'vue/no-v-html': 'off',
-            'vue/no-v-text-v-html-on-component': 'off',
-            'vue/require-toggle-inside-transition': 'off',
-            'vue/require-explicit-emits': 'off',
-            'vue/html-indent': 'off',
-            'vue/max-attributes-per-line': 'off',
-            'vue/html-self-closing': 'off',
-            'tailwindcss/no-custom-classname': 'off',
-            'vue/singleline-html-element-content-newline': 'off',
-            'linebreak-style': 'off',
-            'prettier/prettier': [
-                'error',
-                {
-                    endOfLine: 'auto',
-                    trailingComma: 'es5',
-                    semi: false,
-                    singleQuote: true,
-                    useTabs: false,
-                    quoteProps: 'consistent',
-                    tabWidth: 4,
-                    bracketSpacing: true,
-                    arrowParens: 'always',
-                    printWidth: 100,
-                },
-            ],
+}
+
+export const tailwind = {
+    files: ['**/*.{css,scss,sass,vue,html}'],
+    extends: [
+        ...eslintTailwind.configs['flat/recommended'],
+    ],
+}
+
+export const prettier = {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue,css,scss,sass,html}'],
+    ...eslintPluginPrettierRecommended,
+}
+
+export const rules = {
+    'no-undef': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-function-type': 'off',
+    '@typescript-eslint/no-empty-object-type': 'off',
+    '@typescript-eslint/no-require-imports': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-setup-props-destructure': 'off',
+    'vue/one-component-per-file': 'off',
+    'vue/no-v-html': 'off',
+    'vue/no-v-text-v-html-on-component': 'off',
+    'vue/require-toggle-inside-transition': 'off',
+    'vue/require-explicit-emits': 'off',
+    'vue/html-indent': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/html-self-closing': 'off',
+    'tailwindcss/no-custom-classname': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'linebreak-style': 'off',
+    'prettier/prettier': [
+        'error',
+        {
+            endOfLine: 'auto',
+            trailingComma: 'es5',
+            semi: false,
+            singleQuote: true,
+            useTabs: false,
+            quoteProps: 'consistent',
+            tabWidth: 4,
+            bracketSpacing: true,
+            arrowParens: 'always',
+            printWidth: 100,
         },
-    }
+    ],
+}
+
+export const all = typescriptEslint.config(
+    js,
+    ts,
+    vue,
+    prettier,
+    { rules }
 )
